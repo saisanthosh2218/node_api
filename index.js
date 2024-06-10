2cdd1749dd14b1859785ff77282521a893fff2e7
saisanthosh2218 <saisanthoshbunni@gmail.com>
2024-06-10 18:01:46 +0530

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3005;
 const data = require("./api.json");
+const cors = require("cors");
+app.use(cors());
 
 app.get("/api/foodItems", (req, res) => res.send(data));
 app.listen(port, () => console.log(`listening on port ${port}!`));
