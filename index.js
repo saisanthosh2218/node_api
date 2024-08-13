72d13af184aca1d438a2869f4c389995769c8091
saisanthosh2218 <saisanthoshbunni@gmail.com>
2024-08-13 12:45:09 +0530

    cors added

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3005;
 const data = require("./api.json");
+
+const allowedOrigins = [
+  "http://localhost:3000", // Local development
+  "https://food-delivery-gssk-2218.netlify.app", // Deployed frontend
+];
 const cors = require("cors");
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000", // Local development
-      "https://food-delivery-gssk-2218.netlify.app/Food_Products", // Deployed frontend
-    ],
-  })
-);
+app.use(cors({ origin: allowedOrigins }));
 
 app.get("/api/foodItems", (req, res) => res.send(data));
 app.listen(port, () => console.log(`listening on port ${port}!`));
